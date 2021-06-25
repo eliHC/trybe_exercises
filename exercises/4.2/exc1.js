53912ce868e3551405fd65027ec6f0b028faf385
eli <leonardohcds@gmail.com>
2021-06-24 21:37:06 -0300

    done exercise 4 5 6

--- exercises/4.2/exc1.js
+++ exercises/4.2/exc1.js
@@ -1,22 +1,59 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//exc1 --| for(let index = 0; index < numbers.length; index += 1) {
-//exc1 --|     console.log(numbers[index]);
-//exc1 --| }
-//exc1 --| 
+//1----->   for(let index = 0; index < numbers.length; index += 1) {
+//1----->       console.log(numbers[index]);
+//1----->   }
+//1----->   
 
-//exc2 --| let soma = 0 
-//exc2 --| 
-//exc2 --| for(let index = 0; index < numbers.length; index += 1) {
-//exc2 --|     soma = soma + numbers[index];
-//exc2 --|     console.log(soma);
-//exc2 --| }
-//exc2 --| 
+//2----->   let soma = 0 
+//2----->   
+//2----->   for(let index = 0; index < numbers.length; index += 1) {
+//2----->       soma = soma + numbers[index];
+//2----->       console.log(soma);
+//2----->   }
+//2----->   
 
-let media = 0
+//3----->   let media = 0
+//3----->   
+//3----->   for(let index = 0; index < numbers.length; index += 1) {
+//3----->       media = media + numbers[index];
+//3----->   }
+//3----->   media = media / numbers.length;
+//3----->   console.log(media);
+//3----->   
 
-for(let index = 0; index < numbers.length; index += 1) {
-    media = media + numbers[index];
-}
-media = media / numbers.length;
-console.log(media);
+//4----->    let media = 0
+//4----->    
+//4----->    for(let index = 0; index < numbers.length; index += 1) {
+//4----->        media = media + numbers[index];
+//4----->    }
+//4----->    media = media / numbers.length;
+//4----->
+//4----->    if(media > 20) {
+//4----->        console.log('valor maior que 20');
+//4----->    }else{
+//4----->        console.log('valor menor ou igual a 20');
+//4----->    }
+//4----->
+
+
+   let maior = 0;
+   
+   for(let index = 0; index < numbers.length; index += 1) {
+       if (maior < numbers[index]){
+           maior = + numbers[index];
+       }
+   }
+   console.log(maior)
+   
+
+
+
+
+
+
+//for(let index = 0; index < numbers.length; index += 1) {
+//    if(numbers[index] % 2 != 0){
+//        console.log(numbers[index]);
+//    }
+//}
