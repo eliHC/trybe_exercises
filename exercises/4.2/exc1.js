7afecf5c8309a13833bc9d504fe1b7718c00e99c
eli <leonardohcds@gmail.com>
2021-06-24 22:45:02 -0300

    finish exercises

--- exercises/4.2/exc1.js
+++ exercises/4.2/exc1.js
@@ -37,23 +37,48 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //4----->
 
 
-   let maior = 0;
+//5----->   let maior = 0;
+//5----->   
+//5----->   for(let index = 0; index < numbers.length; index += 1) {
+//5----->       if (maior < numbers[index]){
+//5----->           maior = numbers[index];
+//5----->       }
+//5----->   }
+//5----->   console.log(maior)
    
-   for(let index = 0; index < numbers.length; index += 1) {
-       if (maior < numbers[index]){
-           maior = + numbers[index];
-       }
-   }
-   console.log(maior)
+
+
+
+//6----->   for(let index = 0; index < numbers.length; index += 1) {
+//6----->       if(numbers[index] % 2 != 0){
+//6----->           console.log(numbers[index]);
+//6----->       }
+//6----->   }
+
+
+//7------>   let menor = numbers[0];
+//7------>   
+//7------>   for(let index = 0; index < numbers.length; index += 1) {
+//7------>       if (numbers[index] < menor){
+//7------>           menor = numbers[index];
+//7------>       }
+//7------>   }
+//7------>   console.log(menor);
+
+//8------>   let lista = [];
+//8------>   
+//8------>   for(let index = 0; index <= 25; index +=1 ) {
+//8------>       lista.push(index);
+//8------>   }
+//8------>   console.log(lista);
    
+let lista = [];
 
+for(let index = 0; index <= 25; index +=1 ) {
+    lista.push(index);
+}
 
-
-
-
-
-//for(let index = 0; index < numbers.length; index += 1) {
-//    if(numbers[index] % 2 != 0){
-//        console.log(numbers[index]);
-//    }
-//}
+for(let index = 0; index < lista.length; index +=1 ){
+    let division = lista[index] / 2;
+    console.log(division);
+}
