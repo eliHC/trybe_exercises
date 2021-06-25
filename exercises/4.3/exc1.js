a1652f6fcb9c39c213db44b1dfecf2fe25d022a8
eli <leonardohcds@gmail.com>
2021-06-25 19:14:43 -0300

    somehow i did the first one

--- exercises/4.3/exc1.js
+++ exercises/4.3/exc1.js
@@ -1,13 +1,5 @@
-//1 - contar as *
-//2 - 
-//3 - 
-//4 -  
-
-
-
 let n = 5;
 let star = '*'; 
-
 let line = '';
 
 for (index = 0; index <= n; index += 1) {
@@ -17,3 +9,14 @@ for (index = 0; index <= n; index += 1) {
 
 
 
+
+//2->       let n = 5;
+//2->       let star = '*'; 
+//2->       
+//2->       let line = '';
+//2->       
+//2->       for (index = 0; index <= n; index += 1) {
+//2->           line += star;
+//2->           console.log(line);
+//2->       }
+//2->       
