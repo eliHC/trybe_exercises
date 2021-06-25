814d426aa27dd1051996b3435a325e32db6a0011
eli <leonardohcds@gmail.com>
2021-06-24 22:45:54 -0300

    nada

--- exercises/4.2/exc1.js
+++ exercises/4.2/exc1.js
@@ -72,13 +72,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //8------>   }
 //8------>   console.log(lista);
    
-let lista = [];
-
-for(let index = 0; index <= 25; index +=1 ) {
-    lista.push(index);
-}
-
-for(let index = 0; index < lista.length; index +=1 ){
-    let division = lista[index] / 2;
-    console.log(division);
-}
+//------->  let lista = [];
+//------->  
+//------->  for(let index = 0; index <= 25; index +=1 ) {
+//------->      lista.push(index);
+//------->  }
+//------->  
+//------->  for(let index = 0; index < lista.length; index +=1 ){
+//------->      let division = lista[index] / 2;
+//------->      console.log(division);
+//------->  }
+//------->  
