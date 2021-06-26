e5ecb1aa9e72c50caf4519ee29842b3dd269324f
eli <leonardohcds@gmail.com>
2021-06-26 20:29:52 -0300

    finished exercises 1 and 2

--- exercises/4.3/exc1.js
+++ exercises/4.3/exc1.js
@@ -1,19 +1,26 @@
+//1_>       for (let index = 0; index < n; index += 1) {
+//1_>       
+//1_>           line =  line + stars;
+//1_>       }
+//1_>       for (let index = 0; index < n; index += 1) {
+//1_>           console.log(line);
+//1_>       }
+
+
 //2->       let n = 5;
-//2->       let star = '*'; 
+//2->       let stars = '*'; 
 //2->       
 //2->       let line = '';
 //2->       
 //2->       for (index = 0; index <= n; index += 1) {
-//2->           line += star;
+//2->           line += stars;
 //2->           console.log(line);
 //2->       }
 //2->       
 
 
 let n = 5;
-let star = '*'; 
+let stars = '*';
+let blank = ' ';
 let line = '';
 
-for (index = 0; index <= n; index += 1) {
-    console.log(star + n.length);
-}
