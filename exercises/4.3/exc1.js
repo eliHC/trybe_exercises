be36ba569c638c73eeaabfaec707c63c58bdc8ad
eli <leonardohcds@gmail.com>
2021-06-25 21:17:35 -0300

    checkpoint

--- exercises/4.3/exc1.js
+++ exercises/4.3/exc1.js
@@ -1,15 +1,3 @@
-let n = 5;
-let star = '*'; 
-let line = '';
-
-for (index = 0; index <= n; index += 1) {
-    line += star;
-    console.log(line);
-}
-
-
-
-
 //2->       let n = 5;
 //2->       let star = '*'; 
 //2->       
@@ -20,3 +8,12 @@ for (index = 0; index <= n; index += 1) {
 //2->           console.log(line);
 //2->       }
 //2->       
+
+
+let n = 5;
+let star = '*'; 
+let line = '';
+
+for (index = 0; index <= n; index += 1) {
+    console.log(star + n.length);
+}
