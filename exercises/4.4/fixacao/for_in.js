c3909dcd6de5eac4f46d64d223edf17653e32466
eli <leonardohcds@gmail.com>
2021-06-28 16:11:53 -0300

    fixation of for-in content done

---
+++ exercises/4.4/fixacao/for_in.js
@@ -0,0 +1,22 @@
+//1->       let names = {
+//1->           person1: 'João',
+//1->           person2: 'Maria',
+//1->           person3: 'Jorge' 
+//1->         };
+//1->       
+//1->         for (let key in names) {
+//1->           console.log('Olá ' + names[key]);
+//1->         };
+
+
+//2->       let car = {
+//2->           model: 'A3 Sedan',
+//2->           manufacturer: 'Audi',
+//2->           year: 2020
+//2->         };
+//2->       
+//2->       for (let key in car) {
+//2->           console.log(key, car[key])
+//2->       }
+//2->       
+
