a6e4a149ecfe2f37717b922ad9185f768b0455e4
eli <leonardohcds@gmail.com>
2021-06-28 14:07:22 -0300

    final commit of the day

--- exercises/4.3/exc1.js
+++ exercises/4.3/exc1.js
@@ -21,6 +21,10 @@
 
 let n = 5;
 let stars = '*';
-let blank = ' ';
+let blank = 'o';
 let line = '';
 
+for (let index = 0; index < n; index -= 1) {
+    line = line + blank + stars;
+    console.log(line);
+}
