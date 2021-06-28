cf4c3d661f1d40f45b189acc41421e7de16bf056
eli <leonardohcds@gmail.com>
2021-06-28 18:39:59 -0300

    finish exercise pt1

---
+++ exercises/4.4/exercises/exercises_part1.js
@@ -0,0 +1,44 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info.recorrente = 'sim';
+
+//1>>       
+//1>>         console.log('Bem vinda, ' + info.personagem)
+//1>>       
+//1>>         
+
+
+//2>>       info.recorrente = 'sim';
+//2>>       
+//2>>       console.log(info);
+//2>>       
+
+
+//3>>       for(let key in info){
+//3>>           console.log(key);
+//3>>       }
+//3>>       
+
+
+//4>>       for (let key in info) {
+//4>>           console.log(info[key]);
+//4>>       }
+//4>>       
+
+
+//5>>       let info2 = {
+//5>>           personagem: 'Tio Patinhas',
+//5>>           origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//5>>           nota: 'O último MacPatinhas',
+//5>>           recorrente: 'sim'
+//5>>       }
+//5>>       
+//5>>       for (let key in info, info2) {
+//5>>           console.log(info[key] + ' e ' + info2[key]);
+//5>>       }
+//5>>       
+
