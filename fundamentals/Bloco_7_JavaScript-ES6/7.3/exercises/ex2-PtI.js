53267ee20dd57ea8768d203b70fe6517946260a7
eli <leonardohcds@gmail.com>
2021-07-26 22:15:20 -0300

    cp

--- fundamentals/Bloco_7_JavaScript-ES6/7.3/exercises/ex2-PtI.js
+++ fundamentals/Bloco_7_JavaScript-ES6/7.3/exercises/ex2-PtI.js
@@ -10,4 +10,16 @@ function myRemove(arr, item) {
   return newArr;
 }
 
-// implemente seus testes aqui
+//implemente seus testes aqui
+
+//  1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], 'NAO!');
+
+//  2 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'está retornando o array [1, 2, 3, 4]');
+
+//  3 - Verifique se o array passado por parâmetro não sofreu alterações
+assert.notDeepStrictEqual(myRemove([1,2,3,4], 3),[1,2,3,4], 'array não modificada');
+
+//  4 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5) [1, 2, 3, 4, 5]);
