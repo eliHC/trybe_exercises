ea0d85062d35f5e6e648496287dea9cbd79e930e
eli <leonardohcds@gmail.com>
2021-07-27 09:28:46 -0300

    pt II exercises 1 and 2

---
+++ fundamentals/Bloco_7_JavaScript-ES6/7.1/exercises/ptII.js
@@ -0,0 +1,31 @@
+//  1 - Crie uma função que receba um número e retorne seu fatorial.
+// const factorial = (num) => {
+//   let result = num;
+//   for (index = 1;index < num;index += 1) {
+//     result *= index;
+//   }
+//   return console.log(result);
+// }
+
+// factorial(6);
+
+// ref: https://serprogramador.com.br/artigos/topico/javascript/Como-calcular-a-operacao-matematica-de-fatorial-com-JavaScript
+
+// 2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+// const biggestWord = (phrase) => {
+//   let splitedPhrase = phrase.split(' ');
+//   let currentBiggest = '';
+//   for (i = 0; i < splitedPhrase.length; i += 1) {
+//     if (currentBiggest.length < splitedPhrase[i].length) { 
+//      currentBiggest = splitedPhrase[i];
+//     }
+//   }
+
+//   return console.log(currentBiggest);
+// }
+
+// biggestWord('Antônio morreu no banheiro e não sabemos o que aconteceu');
+
+// ref: https://stackoverflow.com/questions/17386774/javascript-find-longest-word-in-a-string
+
