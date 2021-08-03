26b4c8e1df207fbcee1b595adbe480838f5cb13a
eli <leonardohcds@gmail.com>
2021-08-03 12:29:11 -0300

    exercises 8.4

--- fundamentals/Bloco_8_Higher-Order-Functions-JS-ES6/8.4/exercises.js
+++ fundamentals/Bloco_8_Higher-Order-Functions-JS-ES6/8.4/exercises.js
@@ -2,6 +2,8 @@
 // 1 - Dada uma matriz, transforme em um array.
 
 const assert = require('assert');
+const books = require('./data');
+
 
 // const arrays = [
 //   ['1', '2', '3'],
@@ -16,78 +18,15 @@ const assert = require('assert');
 
 // assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
 
-
-
-
-
-
-
 // Para os exercícios 2, 3 e 4 considere o seguinte array:
 
 // const assert = require('assert');
 
-const books = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
-  {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: {
-      name: 'Stephen King',
-      birthYear: 1947,
-    },
-    releaseYear: 1986,
-  },
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: {
-      name: 'H. P. Lovecraft',
-      birthYear: 1890,
-    },
-    releaseYear: 1928,
-  },
-];
+
+
+
+
+
 
 // Adicione o código do exercício aqui:
 // 2 - Crie uma string com os nomes de todas as pessoas autoras.
@@ -100,28 +39,26 @@ const books = [
 //     if (i === arr.length - 1) {
 //       return `${acc}${curr.author.name}.`;
 //     }
-//       return `${acc}${curr.author.name}, `;
-//   }, [])
+
+//     return `${acc}${curr.author.name}, `;
+//   }, []);
 // };
 
 // assert.strictEqual(reduceNames(), expectedResult);
 
-
-
-
-
-
 // 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
 
-const expectedResult = 43;
+// const expectedResult = 43;
 
-function averageAge() {
-  // escreva seu código aqui
-  const ages = books.reduce((acc, curr) => acc + (curr.releaseYear - curr.author.birthYear), 0);
-  return ages / books.length;
-}
+// function averageAge() {
+//   // escreva seu código aqui
+//   const ages = books.reduce((acc, curr) => acc + (curr.releaseYear - curr.author.birthYear), 0);
+//   return ages / books.length;
+// }
 
-assert.strictEqual(averageAge(), expectedResult);
+// assert.strictEqual(averageAge(), expectedResult);
+
+
 
 
 
@@ -129,31 +66,37 @@ assert.strictEqual(averageAge(), expectedResult);
 
 // 4 - Encontre o livro com o maior nome.
 
+// const expectedResult = {
+//   id: 1,
+//   name: "As Crônicas de Gelo e Fogo",
+//   genre: "Fantasia",
+//   author: {
+//     name: "George R. R. Martin",
+//     birthYear: 1948,
+//   },
+//   releaseYear: 1991,
+// };
 
-const expectedResult = {
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  genre: 'Fantasia',
-  author: {
-    name: 'George R. R. Martin',
-    birthYear: 1948,
-  },
-  releaseYear: 1991,
-};
+// function longestNamedBook() {
+//   // escreva seu código aqui
+//   return books.reduce((accumulator3000, currentT400) => {
+//     if (accumulator3000.name.length < currentT400.name.length) {
+//       return currentT400;
+//     }
 
-function longestNamedBook() {
-  // escreva seu código aqui 
+//     return accumulator3000;
+//   });
+// }
 
-  return books.reduce((acc, cur));
-}
-
-assert.deepStrictEqual(longestNamedBook(), expectedResult);
+// assert.deepStrictEqual(longestNamedBook(), expectedResult);
 
 
 
 
 
-// // 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 
 // const assert = require('assert');
 
@@ -165,9 +108,20 @@ assert.deepStrictEqual(longestNamedBook(), expectedResult);
 
 // function containsA() {
 //   // escreva seu código aqui
+//   let splited =  names.reduce((acc, curr) => acc += curr).split('');
+
+//   return splited.reduce((acc, curr) => (curr.toLowerCase() === 'a'? acc += 1 : acc), 0);
 // }
 
 // assert.deepStrictEqual(containsA(), 20);
+
+
+
+
+
+
+
+
 
 
 
@@ -177,20 +131,30 @@ assert.deepStrictEqual(longestNamedBook(), expectedResult);
 
 // const assert = require('assert');
 
-// const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-// const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-// function studentAverage() {
-//   // escreva seu código aqui
-// }
+function studentAverage() {
+  // escreva seu código aqui
+  const averageGrades = grades.map((element) => element.reduce((acc, curr) => acc + curr) / element.length);
 
-// const expected = [
-//   { name: 'Pedro Henrique', average: 7.8 },
-//   { name: 'Miguel', average: 9.2 },
-//   { name: 'Maria Clara', average: 8.8 },
-// ];
 
-// assert.deepStrictEqual(studentAverage(), expected);
+  return students.reduce((acc, curr, i) => {
+    acc.push({name: curr, average: averageGrades[i]})
+    return acc;
+  }, [])
+}
+
+  
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
+
+
 
 
 
