89191ebaa1307666921975fa4f3287bcb3f5d684
eli <leonardohcds@gmail.com>
2021-08-19 13:56:23 -0300

    exercises 1 and 2

--- fundamentals/Bloco_10_Jest/10.2/tests/exercises.test.js
+++ fundamentals/Bloco_10_Jest/10.2/tests/exercises.test.js
@@ -3,11 +3,12 @@ const {
   users,
   findUserById,
   getUserName,
-} = require('../src/exercises')
+} = require('../src/exercises');
+// test = require('@jest/globals');
 
 
  test('Check if "test" is equal "test"', (done) => {
-  uppercase('test', (theString) =>{
+  uppercase('test', (theString) => {
     try {
       expect(theString).toMatch('TEST');
       done();
@@ -18,6 +19,12 @@ const {
 });
 
 
-test('if the user exists', () => {
+test('if the user exists', () => (
+  getUserName(2).then((user) => expect(user).toEqual('Paul'))
+));
 
-})
+// test('if the user DONT exists', () => {
+//   let val2 = getUserName(2)
+//   getUserName(2).catch((user) => expect(user).toEqual('Pauxl'));
+//   console.log(val2);
+// });
