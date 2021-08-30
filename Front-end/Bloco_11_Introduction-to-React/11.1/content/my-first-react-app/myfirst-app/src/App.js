0ef565066688d4d29e9b868ae87e5c90c83bcde9
eliHC <leonardohcds@gmail.com>
2021-08-30 18:17:04 -0300

    content finished

---
+++ Front-end/Bloco_11_Introduction-to-React/11.1/content/my-first-react-app/myfirst-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import IdontKnow from './Component';
+
+function App() {
+  return <IdontKnow />;
+}
+
+export default App;
---
+++ Front-end/Bloco_11_Introduction-to-React/11.1/content/my-first-react-app/myfirst-app/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class IdontKnow extends React.Component {
+  render (){
+  return (
+  <div>
+    <h1>My Name</h1>
+    <p>lorem ipsum</p> 
+  </div>)
+  }
+};
+
+export default IdontKnow;
